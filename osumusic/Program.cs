1d91959f3d0d7bdbc0d9d026e3a0be4c69b6d9e2
Quicksoapy <73587977+Quicksoapy@users.noreply.github.com>
2021-04-27 09:06:21 +0200

    v1

---
+++ osumusic/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace osumusic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Paste the address of your osu songs folder (by default in %appdata% local):");
+            string addressSongs = Console.ReadLine();
+            if (!Directory.Exists(addressSongs))
+            {
+                Console.WriteLine("That address doesn't exist on this system.");
+                return;
+            }
+            Console.WriteLine("Paste the folder where you want to put the songs:");
+            string addressResult = Console.ReadLine();
+            if (!Directory.Exists(addressResult))
+            {
+                Console.WriteLine("That address doesn't exist on this system.");
+                return;
+            }
+            IEnumerable<string> songsList = Directory.EnumerateDirectories(addressSongs);
+            foreach (var song in songsList)
+            {
+                var mp3path = Directory.GetFiles(song, "*.mp3");
+                var filename = Path.GetFileName(song);
+                File.Copy(mp3path[0], addressResult + "/" + filename + ".mp3");
+            }
+            Console.WriteLine("Files successfully exported, enjoy! :D");
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+    }
+}
