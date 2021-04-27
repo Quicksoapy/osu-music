8f1f3c6c4e087d0e06197c2eef93b21cd52effa8
Quicksoapy <73587977+Quicksoapy@users.noreply.github.com>
2021-04-27 09:32:50 +0200

    yes

--- osumusic/Program.cs
+++ osumusic/Program.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace osumusic
 {
@@ -24,13 +25,24 @@ namespace osumusic
                 Console.WriteLine("That address doesn't exist on this system.");
                 return;
             }
+            Console.WriteLine("Enter 0 for no artwork, press 1 if you want the artwork:");
+            string artworkBool = Console.ReadLine();
             IEnumerable<string> songsList = Directory.EnumerateDirectories(addressSongs);
+
             foreach (var song in songsList)
             {
                 var mp3path = Directory.GetFiles(song, "*.mp3");
                 var filename = Path.GetFileName(song);
-                File.Copy(mp3path[0], addressResult + "/" + filename + ".mp3");
+                if (!File.Exists(addressResult + "/" + filename + ".mp3"))
+                {
+                    if (artworkBool == "0")
+                    {
+                        
+                    }
+                    File.Copy(mp3path[0], addressResult + "/" + filename + ".mp3");
+                }
             }
+            
             Console.WriteLine("Files successfully exported, enjoy! :D");
             Console.WriteLine("Press enter to close...");
             Console.ReadLine();
